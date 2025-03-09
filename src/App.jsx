f79ba28065a9e6baf997305b0ed1678b2d9da027
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-09 23:14:07 +0630

    guess letters

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Header from "./components/Header";
 import Won from "@/components/WonStatus.jsx";
 import { languages } from "../dummy/languages";
 import { useState } from "react";
+import clsx from "clsx";
 
 export default function App() {
   const [currentWord, setCurrentWord] = useState("react");
+  const [guessLetters, setGuessLetters] = useState([]);
 
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+  //mapping for languages
   const languageEl = languages.map((lang) => (
     <span
       className="px-3 py-1 rounded"
@@ -18,6 +21,7 @@ export default function App() {
     </span>
   ));
 
+  //mapping for currentWord
   const letterEl = currentWord.split("").map((letter, index) => (
     <span
       key={index}
@@ -27,9 +31,32 @@ export default function App() {
     </span>
   ));
 
-  const keyboard = alphabet.split('').map((letter,idx) => (
-    <button key={idx} type="button" className="w-10 h-10 flex justify-center items-center bg-[#fcba29] rounded font-semibold text-xl text-gray-600">{letter.toUpperCase()}</button>
-  ))
+  ////mapping for keyboard
+  const keyboard = alphabet.split("").map((letter, idx) => {
+    const isGuessed = guessLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+
+    return (
+      <button
+        key={idx}
+        type="button"
+        onClick={() => addGuessLetter(letter)}
+        className={clsx(
+          "cursor-pointer w-10 h-10 flex justify-center items-center bg-[#fcba29] hover:bg-[#f5d182] rounded font-semibold text-xl text-gray-600",
+          { "bg-green-500": isCorrect, "bg-red-500": isWrong }
+        )}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
+
+  const addGuessLetter = (letter) => {
+    setGuessLetters((prevLetter) =>
+      prevLetter.includes(letter) ? prevLetter : [...prevLetter, letter]
+    );
+  };
 
   return (
     <>
@@ -49,8 +76,14 @@ export default function App() {
           {letterEl}
         </section>
 
-        <section className="flex flex-wrap justify-center items-center gap-3 mt-5 max-w-[450px] mx-auto">
+        <section className="flex flex-wrap justify-center items-center gap-3 mt-5 max-w-[500px] mx-auto">
           {keyboard}
+        </section>
+
+        <section className="newGameBtn flex justify-center mt-5">
+          <button className="bg-blue-500 px-10 py-2 rounded capitalize text-black cursor-pointer hover:bg-blue-400 font-semibold">
+            new game
+          </button>
         </section>
       </main>
     </>
