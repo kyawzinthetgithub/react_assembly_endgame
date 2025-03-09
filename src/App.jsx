d3a92ef0bface4bc7d04748b98cc1c2a564076b6
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-09 15:12:54 +0630

    letter element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 import Header from "./components/Header";
 import Won from "@/components/WonStatus.jsx";
+import { languages } from "../dummy/languages";
+import { useState } from "react";
 
 export default function App() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const languageEl = languages.map((lang) => (
+    <span
+      className="px-3 py-1 rounded"
+      key={lang.name}
+      style={{ background: lang.backgroundColor, color: lang.color }}
+    >
+      {lang.name}
+    </span>
+  ));
+
+  const letterEl = currentWord.split('').map((letter,index) => (
+    <span key={index} className="bg-[#323232] font-semibold flex justify-center items-center px-4 py-2 border-b border-white">{letter.toUpperCase()}</span>
+  ));
+
   return (
     <>
       <main>
-        <Header />
-        <Won />
+        <section className="flex justify-center items-center">
+          <Header />
+        </section>
+        <section className="flex justify-center w-full">
+          <Won />
+        </section>
+
+        <section className="languagesSection flex flex-wrap justify-center items-center gap-3 w- px-10 mb-9">
+          {languageEl}
+        </section>
+
+        <section className="flex justify-center items-center gap-x-3">
+          {letterEl}
+        </section>
+
       </main>
     </>
   );
--- src/components/WonStatus.jsx
+++ src/components/WonStatus.jsx
@@ -1,10 +1,10 @@
 export default function won() {
   return (
     <>
-      <div className="bg-green-500 max-w-[350px] md:max-w-[500px] my-3 flex justify-center items-center p-3">
+      <div className="bg-green-600 w-[300px] md:w-[500px] my-3 flex justify-center items-center p-3 rounded">
         <div>
           <h1 className="text-xl">You Won!</h1>
-          <p>Well done.</p>
+          <p>Well done!🎉</p>
         </div>
       </div>
     </>
