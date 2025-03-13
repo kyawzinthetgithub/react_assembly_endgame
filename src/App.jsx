f03d2611361e998e6e190e02650c8c27910fa332
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-03-13 22:52:19 +0630

    show farware message and disable when game over

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Lost from "@/components/LostStatus.jsx";
 import { languages } from "../dummy/languages";
 import { useState } from "react";
 import clsx from "clsx";
+import { getFarewellText } from "../utils";
 
 export default function App() {
   //state values
@@ -15,9 +16,15 @@ export default function App() {
     (letter) => !currentWord.includes(letter)
   ).length;
 
-  const isGameWon = currentWord.split('').every(letter => guessLetters.includes(letter));
+  const isGameWon = currentWord
+    .split("")
+    .every((letter) => guessLetters.includes(letter));
   const isGameLost = wrongGuesLength >= languages.length - 1;
   const isGameOver = isGameWon || isGameLost;
+
+  const lastGuessLatter = guessLetters[guessLetters.length - 1];
+  const lastGuessIncorrect =
+    lastGuessLatter && !currentWord.includes(lastGuessLatter);
 
   //static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -64,8 +71,9 @@ export default function App() {
         key={idx}
         type="button"
         onClick={() => addGuessLetter(letter)}
+        disabled={isGameOver}
         className={clsx(
-          "cursor-pointer w-10 h-10 flex justify-center items-center bg-[#fcba29] rounded font-semibold text-xl text-gray-600",
+          "cursor-pointer w-10 h-10 flex justify-center items-center bg-[#fcba29] rounded font-semibold text-xl text-gray-600 disabled:cursor-not-allowed disabled:bg-gray-200",
           { "bg-green-500": isCorrect, "bg-red-500": isWrong }
         )}
       >
@@ -89,9 +97,14 @@ export default function App() {
         <section className="flex justify-center w-full">
           {isGameOver && isGameWon && <Won />}
           {isGameOver && isGameLost && <Lost />}
+          {!isGameOver && lastGuessIncorrect && (
+            <p className="bg-purple-500 rounded-md px-10 py-3 border border-dashed border-purple-100 my-5">
+              {getFarewellText(languages[wrongGuesLength - 1].name)}
+            </p>
+          )}
         </section>
 
-        <section className="languagesSection flex flex-wrap justify-center items-center gap-3 w- px-10 mb-9">
+        <section className="languagesSection flex flex-wrap justify-center items-center gap-3 w- px-10 my-10">
           {languageEl}
         </section>
 
@@ -104,9 +117,11 @@ export default function App() {
         </section>
 
         <section className="newGameBtn flex justify-center mt-5">
-          {isGameOver && <button className="bg-blue-500 px-10 py-2 rounded capitalize text-black cursor-pointer hover:bg-blue-400 font-semibold">
-            new game
-          </button>}
+          {isGameOver && (
+            <button className="bg-blue-500 px-10 py-2 rounded capitalize text-black cursor-pointer hover:bg-blue-400 font-semibold">
+              new game
+            </button>
+          )}
         </section>
       </main>
     </>
